672d1d6cfe3cdde62c953aef631feccc78301660
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-06-29 17:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === "susan"){console.log("the name is correct")}
-else {console.log("Expected the name to be Susan")}
